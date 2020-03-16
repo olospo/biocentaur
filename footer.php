b963b1d5e59c01fb0932a1bbc28956882d7d8a6c
Tom Brooks <tom@olospo.co.uk>
2020-03-16 12:34:37 +0000

    Footer updates

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 <footer>
   <div class="container">
-    <a href="#" class="back_to_top">Back to Top</a>
+    <!-- <a href="#" class="back_to_top">Back to Top</a> -->
     <div class="row">
       <div class="about three columns">
         <img src="<?php bloginfo('template_directory'); ?>/img/biocentaur_logo_rev.svg" alt="Biocentaur Logo"/>
@@ -17,18 +17,53 @@
       </div>
     
       <div class="three columns">
-  
+        <ul>
+          <li class="heading"><h5><a href="#">Individuals</a></h5></li>
+          <li><a href="#">Our process</a></li>
+          <li><a href="#">Predictive test</a></li>
+          <li>
+            <a href="#">Support</a>
+            <ul>
+              <li><a href="#">Conditions</a></li>
+              <li><a href="#">Health features</a></li>
+            </ul>
+          </li>
+          <li><a href="#">FAQs</a></li>
+        </ul>
       </div>
       <div class="three columns">
-  
+        <ul>
+          <li class="heading"><h5><a href="#">Clinicians</a></h5></li>
+          <li><a href="#">Clinicians</a></li>
+          <li>
+            <a href="#">Our tests</a>
+            <ul>
+              <li><a href="#">Predictive test</a></li>
+              <li><a href="#">Dianostic test</a></li>
+              <li><a href="#">Targeted test</a></li>
+            </ul>
+          </li>
+          <li><a href="#">FAQs</a></li>
+        </ul>
       </div>
       <div class="three columns">
-  
+        <ul>
+          <li class="heading">
+            <h5><a href="#">About</a></h5>
+            <ul>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Laboratory</a></li>
+            </ul>
+          </li>
+        </ul>
+        <ul>
+          <li class="heading"><h5><a href="#">Contact</a></h5></li>
+        </ul>
       </div>
     </div>
     <div class="row">
-      <div class="twelve columns">
-        Biocentaur &copy; <?php echo date("Y"); ?>
+      <div class="end_links twelve columns">
+        <strong>Biocentaur &copy; <?php echo date("Y"); ?>, all rights reserved</strong> | <a href="#">Disclaimer & copyrights</a> | <a href="#">Privacy & cookie policy</a> | <a href="#">Accessibility</a> | <a href="#">Sitemap</a> | <a href="#">Terms of service</a>
       </div>
     </div>
   </div>
