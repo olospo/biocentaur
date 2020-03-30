0c281412b79a302c5b32ae5b387d5da54597b3cb
Tom Brooks <tom@olospo.co.uk>
2020-03-30 11:54:58 +0100

    Fix customer / contact template error

--- templates/contact.php
+++ templates/contact.php
@@ -1,26 +1,27 @@
-<?php /* Template Name: Customer */
+<?php /* Template Name: Contact */
 get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 
-<section class="page_navigation">
-  <div class="container">
-    
-  </div>
-</section>
-
 <section class="post">
-  <div class="container">
-    <a href="<?php echo get_site_url(); ?>" class="back">Back to Biocentaur</a>
-    <h1>Order your test</h1>
-    <div class="individual_button">
-    
-    </div>
-    <div class="clinician_button">
-      
+  <div class="container flex">
+    <aside class="three columns">
+      <p>
+        <strong>Biocentaur</strong><br />
+        Baarerstrasse 75, Zug, 6300.<br />
+        Switzerland
+      </p>
+      <p>
+        <a href="tel:+441179181506">+44 117 918 1506</a><br />
+        <a href="mailto:info@biocentaur.com">info@biocentaur.com</a>
+      </p>
+    </aside>
+    <div class="content nine columns extra_gutter">
+      <h1><?php the_title(); ?></h1>
+      <?php the_content(); ?>
+      <?php echo do_shortcode('[ninja_form id=1]'); ?>
     </div>
   </div>
 </section>
 
 <?php endwhile; // end of the loop. ?>
-
 <?php get_footer(); ?>
