8c226013089f13e7b7d7363024439b98991fb6df
Tom Brooks <tom@olospo.co.uk>
2020-04-19 14:57:31 +0100

    Fix CPT search

--- functions.php
+++ functions.php
@@ -314,9 +314,6 @@ class childNav extends Walker_page {
   }
 }
 
-// Add CPT to Search
-
-add_filter( 'pre_get_posts', 'cpt_search' );
 /**
  * This function modifies the main WordPress query to include an array of 
  * post types instead of the default 'post' post type.
