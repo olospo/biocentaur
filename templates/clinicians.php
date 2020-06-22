836403110317e4f168e49eee9b3705b233b6b631
Tom Brooks <tom@olospo.co.uk>
2020-06-22 12:44:54 +0100

    Individual and Clinician page fixes

--- templates/clinicians.php
+++ templates/clinicians.php
@@ -7,8 +7,6 @@ while ( have_posts() ) : the_post(); ?>
 
 <?php get_template_part( 'inc/our_tests' ); // Our Tests ?>
 
-<?php get_template_part( 'inc/order_test' ); // Order Test CTA ?>
-
 <?php get_template_part( 'inc/about_bio' ); // About Biocentaur ?>
 
 <?php get_template_part('inc/footer_faq_clinician'); // Footer FAQ ?>
--- templates/individuals.php
+++ templates/individuals.php
@@ -7,12 +7,6 @@ while ( have_posts() ) : the_post(); ?>
 
 <?php get_template_part( 'inc/steps' ); // Animation steps ?>
 
-<?php get_template_part( 'inc/order_test' ); // Order Test CTA ?>
-
-<?php get_template_part('inc/health_conditions'); // Health Conditions Accordion ?>
-
-<?php get_template_part('inc/know_more'); // Know More section ?>
-
 <?php get_template_part('inc/footer_faq'); // Footer FAQ ?>
 
 <?php endwhile; // end of the loop. ?>
