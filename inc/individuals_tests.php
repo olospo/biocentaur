8767d8ba4af7f5ce10d6ddaf3830b2116d32d836
matt-tgos <52203149+matt-tgos@users.noreply.github.com>
2020-10-02 10:30:07 +0100

    Update individuals_tests.php

--- inc/individuals_tests.php
+++ inc/individuals_tests.php
@@ -8,25 +8,25 @@
         <img src="<?php the_field('test_icon_one',169); ?>">
         <h3>PrediSpot genetic test</h3>
         <p>Determine your risk of developing non-cancerous diseases.</p>
-        <a href="/indviduals/our-tests/predispot-genetic-test/" class="button primary">Learn more</a>
+        <a href="<?php echo get_site_url(); ?>/indviduals/our-tests/predispot-genetic-test/" class="button primary">Learn more</a>
       </div>
       <div class="test_block">
         <img src="<?php the_field('test_icon_two',169); ?>">
         <h3>PrimeSpot genetic test</h3>
         <p>Accurately diagnose Lyme Disease and chronic fatigue syndrome.</p>
-        <a href="/indviduals/our-tests/primespot-genetic-test/" class="button primary">Learn more</a>
+        <a href="<?php echo get_site_url(); ?>/indviduals/our-tests/primespot-genetic-test/" class="button primary">Learn more</a>
       </div>
       <div class="test_block">
         <img src="<?php the_field('test_icon_three',169); ?>">
         <h3>SNPs genetic test</h3>
         <p>Accurate screenings to help determine your response to treatment.</p>
-        <a href="/indviduals/our-tests/snps-genetic-test/" class="button primary">Learn more</a>
+        <a href="<?php echo get_site_url(); ?>/indviduals/our-tests/snps-genetic-test/" class="button primary">Learn more</a>
       </div>
       <div class="test_block">
         <img src="<?php the_field('test_icon_four',169); ?>">
         <h3>PaldiSpot genetic test</h3>
         <p>Reliable tests to identify your particular strain of Lyme disease.</p>
-        <a href="/indviduals/our-tests/paldispot-genetic-test/" class="button primary">Learn more</a>
+        <a href="<?php echo get_site_url(); ?>/indviduals/our-tests/paldispot-genetic-test/" class="button primary">Learn more</a>
       </div>
     </div>
   </div>
