ca6e75a5d25bef6cbf431a9cb5b844d5cd3a3461
Tom Brooks <tom@olospo.co.uk>
2020-10-06 12:36:49 +0100

    Bug fixes

--- inc/individuals_tests.php
+++ inc/individuals_tests.php
@@ -5,32 +5,32 @@
     </div>
     <div class="blocks twelve columns">
       <div class="test_block">
-        <img src="<?php the_field('test_icon_one'); ?>">
-        <h3><?php the_field('test_title_one'); ?></h3>
-        <p><?php the_field('test_excerpt_one'); ?></p>
-        <a href="<?php the_field('learn_more_link_one'); ?>" class="button primary">Learn more</a>
-        <a href="<?php the_field('order_test_link_one'); ?>" class="button filled">Order test</a>
+        <img src="<?php the_field('test_icon_one', 211); ?>">
+        <h3><?php the_field('test_title_one', 211); ?></h3>
+        <p><?php the_field('test_excerpt_one', 211); ?></p>
+        <a href="<?php the_field('learn_more_link_one', 211); ?>" class="button primary">Learn more</a>
+        <!-- <a href="<?php the_field('order_test_link_one', 211); ?>" class="button filled">Order test</a> -->
       </div>
       <div class="test_block">
-        <img src="<?php the_field('test_icon_two'); ?>">
-        <h3><?php the_field('test_title_two'); ?></h3>
-        <p><?php the_field('test_excerpt_two'); ?></p>
-        <a href="<?php the_field('learn_more_link_two'); ?>" class="button primary">Learn more</a>
-        <a href="<?php the_field('order_test_link_two'); ?>" class="button filled">Order test</a>
+        <img src="<?php the_field('test_icon_two', 211); ?>">
+        <h3><?php the_field('test_title_two', 211); ?></h3>
+        <p><?php the_field('test_excerpt_two', 211); ?></p>
+        <a href="<?php the_field('learn_more_link_two', 211); ?>" class="button primary">Learn more</a>
+        <!-- <a href="<?php the_field('order_test_link_two', 211); ?>" class="button filled">Order test</a> -->
       </div>
       <div class="test_block">
-        <img src="<?php the_field('test_icon_three'); ?>">
-        <h3><?php the_field('test_title_three'); ?></h3>
-        <p><?php the_field('test_excerpt_three'); ?></p>
-        <a href="<?php the_field('learn_more_link_three'); ?>" class="button primary">Learn more</a>
-        <a href="<?php the_field('order_test_link_three'); ?>" class="button filled">Order test</a>
+        <img src="<?php the_field('test_icon_three', 211); ?>">
+        <h3><?php the_field('test_title_three', 211); ?></h3>
+        <p><?php the_field('test_excerpt_three', 211); ?></p>
+        <a href="<?php the_field('learn_more_link_three', 211); ?>" class="button primary">Learn more</a>
+        <!-- <a href="<?php the_field('order_test_link_three', 211); ?>" class="button filled">Order test</a> -->
       </div>
       <div class="test_block">
-        <img src="<?php the_field('test_icon_four'); ?>">
-        <h3><?php the_field('test_title_four'); ?></h3>
-        <p><?php the_field('test_excerpt_four'); ?></p>
-        <a href="<?php the_field('learn_more_link_four'); ?>" class="button primary">Learn more</a>
-        <a href="<?php the_field('order_test_link_four'); ?>" class="button filled">Order test</a>
+        <img src="<?php the_field('test_icon_four', 211); ?>">
+        <h3><?php the_field('test_title_four', 211); ?></h3>
+        <p><?php the_field('test_excerpt_four', 211); ?></p>
+        <a href="<?php the_field('learn_more_link_four', 211); ?>" class="button primary">Learn more</a>
+        <!-- <a href="<?php the_field('order_test_link_four', 211); ?>" class="button filled">Order test</a> -->
       </div>
     </div>
   </div>
