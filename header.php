a45f335c1080fadf5f551f20f55195c49d33dc25
matt-tgos <52203149+matt-tgos@users.noreply.github.com>
2020-11-12 14:00:02 +0000

    Update header.php

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
 <?php if( get_field('social_metadata', 'options') ): the_field('social_metadata', 'options'); endif; // Social Metadata ?>
 <?php if( get_field('google_analytics', 'options') ): the_field('google_analytics', 'options'); endif; // Google Analytics Code ?>
 <meta name="google-site-verification" content="add-content-here" />
+<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/89164f605b17cd38eb1f003b2/120e0ecb41cee3aa248b58e19.js");</script>  
 </head>
 <body <?php body_class(); ?>>
 <div class="fixed_header">
