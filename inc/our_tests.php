cca74511db786bc551100dc5be11b748d0e88af9
Tom Brooks <tom@olospo.co.uk>
2021-05-06 11:51:33 +0000

    URL update

--- inc/our_tests.php
+++ inc/our_tests.php
@@ -9,28 +9,28 @@
         <h3><?php the_field('test_title_one',169); ?></h3>
         <p><?php the_field('test_excerpt_one',169); ?></p>
         <a href="<?php the_field('learn_more_link_one',169); ?>" class="button primary">Learn more</a>
-        <a href="<?php the_permalink(); ?>/?add-to-cart=<?php the_field('order_id_one',169); ?>" class="button filled">Add to basket</a>
+        <a href="<?php the_permalink(); ?>?add-to-cart=<?php the_field('order_id_one',169); ?>" class="button filled">Add to basket</a>
       </div>
       <div class="test_block">
         <img src="<?php the_field('test_icon_two',169); ?>">
         <h3><?php the_field('test_title_two',169); ?></h3>
         <p><?php the_field('test_excerpt_two',169); ?></p>
         <a href="<?php the_field('learn_more_link_two',169); ?>" class="button primary">Learn more</a>
-        <a href="<?php the_permalink(); ?>/?add-to-cart=<?php the_field('order_id_two',169); ?>" class="button filled">Add to basket</a>
+        <a href="<?php the_permalink(); ?>?add-to-cart=<?php the_field('order_id_two',169); ?>" class="button filled">Add to basket</a>
       </div>
       <div class="test_block">
         <img src="<?php the_field('test_icon_three',169); ?>">
         <h3><?php the_field('test_title_three',169); ?></h3>
         <p><?php the_field('test_excerpt_three',169); ?></p>
         <a href="<?php the_field('learn_more_link_three',169); ?>" class="button primary">Learn more</a>
-        <a href="<?php the_permalink(); ?>/?add-to-cart=<?php the_field('order_id_three',169); ?>" class="button filled">Add to basket</a>
+        <a href="<?php the_permalink(); ?>?add-to-cart=<?php the_field('order_id_three',169); ?>" class="button filled">Add to basket</a>
       </div>
       <div class="test_block">
         <img src="<?php the_field('test_icon_four',169); ?>">
         <h3><?php the_field('test_title_four',169); ?></h3>
         <p><?php the_field('test_excerpt_four',169); ?></p>
         <a href="<?php the_field('learn_more_link_four',169); ?>" class="button primary">Learn more</a>
-        <a href="<?php the_permalink(); ?>/?add-to-cart=<?php the_field('order_id_four',169); ?>" class="button filled">Add to basket</a>
+        <a href="<?php the_permalink(); ?>?add-to-cart=<?php the_field('order_id_four',169); ?>" class="button filled">Add to basket</a>
       </div>
     </div>
   </div>
